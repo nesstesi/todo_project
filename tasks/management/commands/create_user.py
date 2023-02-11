6150f2d927ca365807ade997b5f87d8f88f33c90
Anastasiia <nastyushka.grebenyuk@gmail.com>
2023-02-11 17:07:55 +0200

    add home page and sign up func

---
+++ tasks/management/commands/create_user.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    help = 'Create new regular user'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--username',
+            action='store',
+        )
+
+    def handle(self, *args, **options):
+        username = options['username']
+        mail = input('Enter email: ')
+        password = input('Enter password: ')
+        User.objects.create_user(username, mail, password)
+
